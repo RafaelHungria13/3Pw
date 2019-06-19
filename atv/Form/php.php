df9a2bfd4611b395772d2732002d292f4ffe6437
NicoleMendes11 <kolemenger5@gmail.com>
2019-06-19 10:39:33 -0300

    first commit

---
+++ atv/Form/php.php
@@ -0,0 +1,41 @@
+<style type="text/css">
+*{
+	text-align: center;
+	width: 550px;
+	margin: 15 auto;
+	font-family: Arial;
+	font-size: 18px;
+	border-color: black;
+	border-radius: 5px;
+	
+}
+fieldset{
+	background-color: 	#7FFFD4;
+}
+
+
+</style>
+
+
+<?php
+	echo "<meta charset=utf-8>";
+	echo "<center>";
+	echo "<h2>FORMULÁRIO</h2>";
+	echo "<legend><b>Qual a melhor forma de procurar estágio?</b></legend>";
+	echo "</center>";
+
+	echo "<center>";
+	echo "<fieldset>";
+	echo "Diretamente nas empresas(local ou site)  24,7%";
+	echo "<br>";
+	echo "Através de eventos e sites de recrutamento e/ou voltados a universitários  19,37%";
+	echo "<br>";
+	echo "Procurar a Central de Estágio da faculdade  19,96%";
+	echo "<br>";
+	echo "Indicação de amigos  30,24%";
+	echo "<br>";
+	echo "Criar um site com seu próprio currículo para ampliar a divulgação de seus objetivos profissionais  5,73%";
+	echo "<br>";
+	echo "</fieldset>";
+	echo "</center>";
+?>
